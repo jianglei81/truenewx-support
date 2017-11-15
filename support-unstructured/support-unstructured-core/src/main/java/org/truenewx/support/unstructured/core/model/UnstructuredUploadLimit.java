3f36e29e7913cf808f468c57e73f4fef93169edd
jianglei <jianglei@jianglei-HP>
2017-11-15 17:11:52 +0800

    上传限制中进行扩展名至MimeType的转换

--- support-unstructured/support-unstructured-core/src/main/java/org/truenewx/support/unstructured/core/model/UnstructuredUploadLimit.java
+++ support-unstructured/support-unstructured-core/src/main/java/org/truenewx/support/unstructured/core/model/UnstructuredUploadLimit.java
@@ -2,6 +2,8 @@ package org.truenewx.support.unstructured.core.model;
 
 import org.springframework.util.Assert;
 
+import com.aliyun.oss.internal.Mimetypes;
+
 /**
  * 非结构化存储上传限制
  *
@@ -13,14 +15,21 @@ public class UnstructuredUploadLimit {
     private int number;
     private long capacity;
     private String[] extensions;
+    private String[] mimeTypes;
 
     public UnstructuredUploadLimit(final int number, final long capacity,
             final String... extensions) {
-        Assert.isTrue(number > 0, "number must be larger than 0");
+        Assert.isTrue(number >= 0, "number must be not less than 0");
         this.number = number;
-        Assert.isTrue(capacity > 0, "capacity must be larger than 0");
+        Assert.isTrue(capacity >= 0, "capacity must be not less than 0");
         this.capacity = capacity;
         this.extensions = extensions;
+        this.mimeTypes = new String[extensions.length];
+        final Mimetypes mimeTypes = Mimetypes.getInstance();
+        for (int i = 0; i < extensions.length; i++) {
+            final String extension = "temp." + extensions[i];
+            this.mimeTypes[i] = mimeTypes.getMimetype(extension);
+        }
     }
 
     public int getNumber() {
@@ -33,6 +42,10 @@ public class UnstructuredUploadLimit {
 
     public String[] getExtensions() {
         return this.extensions;
+    }
+
+    public String[] getMimeTypes() {
+        return this.mimeTypes;
     }
 
 }
