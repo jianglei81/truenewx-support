0ce1c4d109312b4c33cfda20b462e8b2efe499cb
jiangnanyuzi <jiangnanyuzi@163.com>
2018-07-26 11:25:47 +0800

    适应菜单项区分动作型/非动作型

--- support-log/support-log-web/src/main/java/org/truenewx/support/log/web/interceptor/AbstractActionLogInterceptor.java
+++ support-log/support-log-web/src/main/java/org/truenewx/support/log/web/interceptor/AbstractActionLogInterceptor.java
@@ -28,6 +28,7 @@ import org.truenewx.support.log.data.model.UrlAction;
 import org.truenewx.support.log.service.ActionLogWriter;
 import org.truenewx.support.log.web.annotation.LogExcluded;
 import org.truenewx.web.http.HttpLink;
+import org.truenewx.web.menu.model.ActableMenuItem;
 import org.truenewx.web.menu.model.Menu;
 import org.truenewx.web.menu.model.MenuItem;
 import org.truenewx.web.rpc.RpcPort;
@@ -51,12 +52,12 @@ public abstract class AbstractActionLogInterceptor<K extends Serializable>
     private Collection<HttpLink> excludedUrlPatterns;
     private Collection<RpcPort> excludedRpcPatterns;
 
-    public void setExcludedUrlPatterns(final String[] excludedUrlPatterns) {
+    public void setExcludedUrlPatterns(String[] excludedUrlPatterns) {
         if (excludedUrlPatterns != null) {
             this.excludedUrlPatterns = new ArrayList<>();
-            for (final String pattern : excludedUrlPatterns) {
-                final String[] pair = pattern.split(Strings.SEMICOLON);
-                final String url = pair[0];
+            for (String pattern : excludedUrlPatterns) {
+                String[] pair = pattern.split(Strings.SEMICOLON);
+                String url = pair[0];
                 HttpMethod method = null;
                 if (pair.length > 1) {
                     method = EnumUtils.getEnum(HttpMethod.class, pair[1]);
@@ -66,26 +67,26 @@ public abstract class AbstractActionLogInterceptor<K extends Serializable>
         }
     }
 
-    public void setExcludedRpcPatterns(final String[] excludedRpcPatterns) {
+    public void setExcludedRpcPatterns(String[] excludedRpcPatterns) {
         if (excludedRpcPatterns != null) {
             this.excludedRpcPatterns = new ArrayList<>();
-            for (final String pattern : excludedRpcPatterns) {
-                final String[] pair = pattern.split("\\.");
-                final String beanId = pair[0];
+            for (String pattern : excludedRpcPatterns) {
+                String[] pair = pattern.split("\\.");
+                String beanId = pair[0];
                 String methodName = Strings.ASTERISK;
                 Integer argCount = null;
                 if (pair.length > 1) {
-                    final int index = pair[1].indexOf(Strings.LEFT_BRACKET);
+                    int index = pair[1].indexOf(Strings.LEFT_BRACKET);
                     if (index < 0) {
                         methodName = pair[1];
                     } else {
                         methodName = pair[1].substring(0, index);
-                        final String argString = pair[1].substring(index + 1,
+                        String argString = pair[1].substring(index + 1,
                                 pair[1].indexOf(Strings.RIGHT_BRACKET));
                         argCount = argString.split(Strings.COMMA).length;
                     }
                 }
-                final RpcPort rpcPort = argCount == null ? new RpcPort(beanId, methodName)
+                RpcPort rpcPort = argCount == null ? new RpcPort(beanId, methodName)
                         : new RpcPort(beanId, methodName, argCount);
                 this.excludedRpcPatterns.add(rpcPort);
             }
@@ -93,17 +94,17 @@ public abstract class AbstractActionLogInterceptor<K extends Serializable>
     }
 
     @Override
-    public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response,
-            final Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+            Object handler) throws Exception {
         return true;
     }
 
-    protected boolean matches(final String url, final HttpMethod method) {
+    protected boolean matches(String url, HttpMethod method) {
         if (url.startsWith("/rpc/")) { // 忽略所有RPC请求
             return false;
         }
         if (this.excludedUrlPatterns != null) {
-            for (final HttpLink pattern : this.excludedUrlPatterns) {
+            for (HttpLink pattern : this.excludedUrlPatterns) {
                 if (pattern.matches(url, method)) {
                     return false;
                 }
@@ -113,28 +114,28 @@ public abstract class AbstractActionLogInterceptor<K extends Serializable>
     }
 
     @Override
-    public void postHandle(final HttpServletRequest request, final HttpServletResponse response,
-            final Object handler, final ModelAndView modelAndView) throws Exception {
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            ModelAndView modelAndView) throws Exception {
         // 请求失败或为include请求则忽略当前拦截器
         if (response.getStatus() != HttpServletResponse.SC_OK
                 || WebUtils.isIncludeRequest(request)) {
             return;
         }
-        final K userId = getUserId();
+        K userId = getUserId();
         if (userId != null) { // 已登录的才需要记录日志
-            final String url = WebUtil.getRelativeRequestUrl(request);
-            final HttpMethod method = EnumUtils.getEnum(HttpMethod.class, request.getMethod());
+            String url = WebUtil.getRelativeRequestUrl(request);
+            HttpMethod method = EnumUtils.getEnum(HttpMethod.class, request.getMethod());
             if (matches(url, method) && handler instanceof HandlerMethod) { // URL匹配才进行校验
-                final HandlerMethod hm = (HandlerMethod) handler;
-                final LogExcluded logExcluded = hm.getMethodAnnotation(LogExcluded.class);
+                HandlerMethod hm = (HandlerMethod) handler;
+                LogExcluded logExcluded = hm.getMethodAnnotation(LogExcluded.class);
                 if (logExcluded != null && logExcluded.value()) { // 整个方法都被排除
                     return;
                 }
                 // 在创建线程提交执行之前获取菜单和请求参数，以免线程执行环境无法获取
-                final Menu menu = getMenu();
-                final String caption = getUrlActionCaption(menu, url, method, hm);
+                Menu menu = getMenu();
+                String caption = getUrlActionCaption(menu, url, method, hm);
                 if (StringUtils.isNotBlank(caption)) {
-                    final Map<String, Object> params;
+                    Map<String, Object> params;
                     if (logExcluded != null) {
                         params = WebUtil.getRequestParameterMap(request, logExcluded.excluded());
                     } else {
@@ -143,7 +144,7 @@ public abstract class AbstractActionLogInterceptor<K extends Serializable>
                     this.executor.execute(new Runnable() {
                         @Override
                         public void run() {
-                            final UrlAction action = new UrlAction();
+                            UrlAction action = new UrlAction();
                             action.setUrl(url);
                             action.setMethod(method.name());
                             action.setCaption(caption);
@@ -158,19 +159,22 @@ public abstract class AbstractActionLogInterceptor<K extends Serializable>
         }
     }
 
-    private String getUrlActionCaption(final Menu menu, final String url, final HttpMethod method,
-            final HandlerMethod handlerMethod) {
+    private String getUrlActionCaption(Menu menu, String url, HttpMethod method,
+            HandlerMethod handlerMethod) {
         if (menu != null) {
-            final StringBuffer caption = new StringBuffer();
-            final List<Binate<Integer, MenuItem>> indexes = menu.indexesOfItems(url, method);
-            for (final Binate<Integer, MenuItem> binate : indexes) {
-                caption.append(" / ").append(binate.getRight().getCaption());
+            StringBuffer caption = new StringBuffer();
+            List<Binate<Integer, MenuItem>> indexes = menu.indexesOfItems(url, method);
+            for (Binate<Integer, MenuItem> binate : indexes) {
+                MenuItem item = binate.getRight();
+                if (item instanceof ActableMenuItem) {
+                    caption.append(" / ").append(((ActableMenuItem) item).getCaption());
+                }
             }
             if (StringUtils.isNotBlank(caption)) {
                 return caption.toString().trim();
             }
             // 如果无法从菜单中获得显示名称，则尝试从@Caption注解中获取，这意味着使用@Caption注解的方法将被记录操作日志
-            final Caption captionAnnotation = handlerMethod.getMethodAnnotation(Caption.class);
+            Caption captionAnnotation = handlerMethod.getMethodAnnotation(Caption.class);
             if (captionAnnotation != null) {
                 return captionAnnotation.value();
             }
@@ -179,39 +183,36 @@ public abstract class AbstractActionLogInterceptor<K extends Serializable>
     }
 
     @Override
-    public void afterCompletion(final HttpServletRequest request,
-            final HttpServletResponse response, final Object handler, final Exception ex)
-            throws Exception {
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+            Object handler, Exception ex) throws Exception {
     }
 
     @Override
-    public void beforeInvoke(final String beanId, final Method method, final Object[] args)
-            throws Exception {
+    public void beforeInvoke(String beanId, Method method, Object[] args) throws Exception {
     }
 
     @Override
-    public void afterInvoke(final String beanId, final Method method, final Object[] args,
-            final Object result) {
-        final K userId = getUserId();
+    public void afterInvoke(String beanId, Method method, Object[] args, Object result) {
+        K userId = getUserId();
         if (userId != null) { // 已登录的才需要记录日志
-            final String methodName = method.getName();
-            final int argCount = args.length;
+            String methodName = method.getName();
+            int argCount = args.length;
             if (matches(beanId, methodName, argCount)) {
-                final LogExcluded logExcluded = method.getAnnotation(LogExcluded.class);
+                LogExcluded logExcluded = method.getAnnotation(LogExcluded.class);
                 if (logExcluded != null && logExcluded.value()) { // 整个方法都被排除
                     return;
                 }
                 // 在创建线程提交执行之前获取菜单，以免线程执行环境无法获取当前菜单
-                final Menu menu = getMenu();
-                final String caption = getRpcMethodCaption(menu, beanId, method, argCount);
+                Menu menu = getMenu();
+                String caption = getRpcMethodCaption(menu, beanId, method, argCount);
                 if (StringUtils.isNotBlank(caption)) { // 找得到对应显示名称的才记录日志
                     this.executor.execute(new Runnable() {
                         @Override
                         public void run() {
-                            final RpcAction action = new RpcAction();
+                            RpcAction action = new RpcAction();
                             action.setBeanId(beanId);
                             action.setMethodName(methodName);
-                            final List<Object> argList = Arrays.asList(args);
+                            List<Object> argList = Arrays.asList(args);
                             if (!argList.isEmpty()) {
                                 action.setArgs(argList);
                             }
@@ -224,10 +225,9 @@ public abstract class AbstractActionLogInterceptor<K extends Serializable>
         }
     }
 
-    protected boolean matches(final String beanId, final String methodName,
-            final Integer argCount) {
+    protected boolean matches(String beanId, String methodName, Integer argCount) {
         if (this.excludedRpcPatterns != null) {
-            for (final RpcPort pattern : this.excludedRpcPatterns) {
+            for (RpcPort pattern : this.excludedRpcPatterns) {
                 if (pattern.matches(beanId, methodName, argCount)) {
                     return false;
                 }
@@ -236,20 +236,22 @@ public abstract class AbstractActionLogInterceptor<K extends Serializable>
         return true;
     }
 
-    private String getRpcMethodCaption(final Menu menu, final String beanId, final Method method,
-            final int argCount) {
+    private String getRpcMethodCaption(Menu menu, String beanId, Method method, int argCount) {
         if (menu != null) {
-            final StringBuffer caption = new StringBuffer();
-            final List<Binate<Integer, MenuItem>> indexes = menu.indexesOfItems(beanId,
-                    method.getName(), argCount);
-            for (final Binate<Integer, MenuItem> binate : indexes) {
-                caption.append(" / ").append(binate.getRight().getCaption());
+            StringBuffer caption = new StringBuffer();
+            List<Binate<Integer, MenuItem>> indexes = menu.indexesOfItems(beanId, method.getName(),
+                    argCount);
+            for (Binate<Integer, MenuItem> binate : indexes) {
+                MenuItem item = binate.getRight();
+                if (item instanceof ActableMenuItem) {
+                    caption.append(" / ").append(((ActableMenuItem) item).getCaption());
+                }
             }
             if (StringUtils.isNotBlank(caption)) {
                 return caption.toString().trim();
             }
             // 如果无法从菜单中获得显示名称，则尝试从@Caption注解中获取，这意味着使用@Caption注解的方法将被记录操作日志
-            final Caption captionAnnotation = method.getAnnotation(Caption.class);
+            Caption captionAnnotation = method.getAnnotation(Caption.class);
             if (captionAnnotation != null) {
                 return captionAnnotation.value();
             }
